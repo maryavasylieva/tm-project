6b42a2e728abf1fe7d8730dfaac51171a453e619
maryavasylieva <miheta.laurelin@gmail.com>
2021-08-27 22:32:08 +0300

    feat: add typescript & types react-dom/react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,9 @@ import {publicRoutes} from "./Components/routes";
 
 const App = () => {
   return (
+
     <Suspense fallback={<div>Loading...</div>}>
+      <h1>Hello World!</h1>
       <Switch>
         {publicRoutes.map((route) => (
           <Route key={route.path} path={route.path} exact={route.exact}>
